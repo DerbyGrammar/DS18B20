03a415dad264fdddf8de92a48a3516c8dd4c6df5
Chris <c.nethercott@icloud.com>
2016-12-21 21:38:55 +0000

    Added DiscoverDevices.ino

---
+++ src/DiscoverDevices/DiscoverDevices.ino
@@ -0,0 +1,41 @@
+#include <OneWire.h>
+
+void setup() {
+  Serial.begin(9600);
+
+  discoverOneWireDevices();
+}
+
+void loop() {}
+
+void discoverOneWireDevices() {
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+
+  Serial.print();
+  while(ourBus.search(addr)) {
+    Serial.print();
+    for( i = 0; i < 8; i++) {
+      Serial.print();
+      if (addr[i] < 16) {
+        Serial.print();
+      }
+      Serial.print(addr[i], HEX);
+      if (i < 7) {
+        Serial.print();
+      }
+    }
+    if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.print();
+      return;
+    }
+  }
+  Serial.println();
+  Serial.print();
+  ourBus.reset_search();
+  return;
+}
+
+
