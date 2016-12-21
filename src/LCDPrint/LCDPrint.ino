63d54c44a3b6538971aad9c75e68d75278eeabe0
Chris Nethercott <c.nethercott@icloud.com>
2016-12-21 20:56:16 +0000

    Added the relevant libraries for the I2C lcd

--- src/LCDPrint/LCDPrint.ino
+++ src/LCDPrint/LCDPrint.ino
@@ -1,5 +1,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 OneWire oneWire(2); //connect data to pin 2
 DallasTemperature sensors(&oneWire);
@@ -8,6 +10,7 @@ DeviceAddress black = { 0x28, 0xFF, 0x1A, 0xAA, 0x62, 0x15, 0x03, 0x20 };
 DeviceAddress red   = { 0x28, 0xFF, 0xA9, 0xB4, 0x62, 0x15, 0x03, 0x0C }; 
 DeviceAddress green = { 0x28, 0xFF, 0xF5, 0xB3, 0x62, 0x15, 0x03, 0x3E };
 
+LiquidCrystal lcd(0x27, 16, 2);
 
 const int delayTime = 500;
 
